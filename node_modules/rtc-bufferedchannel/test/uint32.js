3caff9c941487d1fe4e94becc9623d2c05396398
Will Ballard <wballard@mailframe.net>
2014-04-30 19:40:34 -0400

    updated node_modules since they are cached

---
+++ node_modules/rtc-bufferedchannel/test/uint32.js
@@ -0,0 +1,105 @@
+var test = require('tape');
+var bufferedchannel = require('..');
+var peerpair = require('peerpair');
+var createArray = require('./helpers/create-uintarray');
+var peers = peerpair();
+var channels = [];
+var bcs = [];
+var checkChunks = require('./helpers/check-chunks')(channels, bcs);
+var smallArray = createArray(Uint32Array, Math.pow(2, 32) - 1, 100);
+var largeArray = createArray(Uint32Array, Math.pow(2, 32) - 1, 45 * 1024);
+var massiveArray = createArray(Uint32Array, Math.pow(2, 32) - 1, 1035 * 1024);
+
+test('uint32: create test connections', function(t) {
+  t.plan(2);
+  peers.createChannelsAndConnect('test', function(err, dcs) {
+    t.ifError(err);
+
+    channels.push(dcs[0], dcs[1]);
+    t.equal(channels.length, 2, 'have two channels');
+  });
+});
+
+test('uint32: have dc connectivity', function(t) {
+  t.plan(1);
+
+  channels[1].onmessage = function(evt) {
+    t.equal(evt.data, 'hi', 'got hi from channel:0');
+    channels[1].onmessage = null;
+  };
+
+  channels[0].send('hi');
+});
+
+test('uint32: create buffered channels for existing channels', function(t) {
+  t.plan(2);
+
+  channels.forEach(function(dc, index) {
+    bcs[index] = bufferedchannel(dc, { maxsize: 16 * 1024 });
+  });
+
+  t.equal(bcs.length, 2, 'created 2 buffered channels');
+  t.equal(typeof bcs[0].send, 'function', 'buffered channels have a send function');
+});
+
+test('uint32: small array is chunked', checkChunks(smallArray, 1, 'uint32'));
+test('uint32: small array is sent ok', function(t) {
+  t.plan(1);
+
+  bcs[1].once('data', function(data) {
+    var equal = true;
+    for (var ii = data.length; equal && ii--; ) {
+      equal = data[ii] === smallArray[ii];
+    }
+
+    t.ok(equal, 'small array passed through correctly');
+  });
+
+  bcs[0].send(smallArray);
+});
+
+test('uint32: large array is chunked', checkChunks(largeArray, 12, 'uint32'));
+test('uint32: large array is sent ok', function(t) {
+  t.plan(1);
+
+  bcs[1].once('data', function(data) {
+    var equal = true;
+    for (var ii = data.length; equal && ii--; ) {
+      equal = data[ii] === largeArray[ii];
+      if (! equal) {
+        console.log('failed at element: ' + ii);
+        console.log(data.length);
+        console.log(largeArray.length);
+        console.log(data[ii], largeArray[ii]);
+        console.log(data);
+      }
+    }
+
+    t.ok(equal, 'large array passed through correctly');
+  });
+
+  bcs[0].send(largeArray);
+});
+
+
+test('uint32: massive array is chunked', checkChunks(massiveArray, 259, 'uint32'));
+test('uint32: massive array is sent ok', function(t) {
+  t.plan(1);
+
+  bcs[1].once('data', function(data) {
+    var equal = true;
+    for (var ii = data.length; equal && ii--; ) {
+      equal = data[ii] === massiveArray[ii];
+      // if (! equal) {
+      //   console.log('failed at element: ' + ii);
+      //   console.log(data.length);
+      //   console.log(largeArray.length);
+      //   console.log(data);
+      // }      
+    }
+
+    t.ok(equal, 'large array passed through correctly');
+  });
+
+  bcs[0].send(massiveArray);
+});
